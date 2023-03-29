307199e944212d9b2ee1fb6cf2a88fdc4dd119a5
Jordi Cenzano Ferret <jcenzano@fb.com>
2023-03-29 18:14:55 +0900

    Simplified encoder. Removed sending queue

--- src-encoder/muxer_sender.js
+++ src-encoder/muxer_sender.js
@@ -7,8 +7,6 @@ LICENSE file in the root directory of this source tree.
 
 const WORKER_PREFIX = "[MUXER-SEND]";
 
-const VIDEO_PENALTY_SENT_FRAMES = 5;
-
 importScripts('utils.js');
 importScripts('../utils/packager_version.js');
 importScripts('../packager/media_packager_header.js');
@@ -17,14 +15,7 @@ let chunks_delivered = 0;
 let workerState = StateEnum.Created;
 
 // Default values
-let audioMaxMaxQueueSizeMs = 300;
-let videoMaxMaxQueueSizeMs = 150;
 let maxAgeChunkS = 120;
-// Video penalty
-// In case of congestion make sure we can send N audios until we can send video again
-let videoPenaltyAudioSentFrames = 0;
-
-let videoNextNeedsKey = false;
 
 const maxFlightRequests = {
     'audio': 4,
@@ -34,22 +25,12 @@ const maxFlightRequests = {
 let urlHostPort = "";
 let urlPath = "";
 
-// Sending queues
-const queue = {
-    'video': [],
-    'audio': []
-}
-
 // Inflight req abort signal
 const abortController = new AbortController();
 const inFlightRequests = {
     'video': {},
     'audio': {}
 };
-
-// Checks if audio and / or video are present
-let isAudioPresent = false;
-let isVideoPresent = false;
 
 // WebTransport data
 let wTtransport = null;
@@ -70,45 +51,10 @@ let efficiencyData = {
 }
 
 // Debugging data 
-let totalAudioSkippedDur = 0;
+/*let totalAudioSkippedDur = 0;
 let totalVideoSkippedDur = 0;
 let numAudioFramesDropped = 0;
-let numVideoFramesDropped = 0;
-
-function getQueueStats(queue) {
-    let oldestTs = -1;
-    let newestTs = -1;
-    let oldestSeqId = -1;
-    let newestSeqId = -1;
-    if (queue.length > 0) {
-        oldestTs = queue[0].chunk.timestamp;
-        oldestSeqId = queue[0].seqId;
-        if (queue.length > 1) {
-            newestTs = queue[queue.length - 1].chunk.timestamp;
-            newestSeqId = queue[queue.length - 1].seqId;
-        }
-    }
-    return {
-        oldestTs: oldestTs,
-        newestTs: newestTs,
-        oldestSeqId: oldestSeqId,
-        newestSeqId: newestSeqId,
-        lengthMs: getQueueLengthMs(queue),
-        numElements: queue.length,
-    }
-}
-
-function getQueueLengthMs(queue) {
-    let r = 0;
-
-    if (queue.length > 1) {
-        const newest = queue[queue.length - 1];
-        const oldest = queue[0];
-
-        r = (newest.chunk.timestamp - oldest.chunk.timestamp) / 1000
-    }
-    return r;
-}
+let numVideoFramesDropped = 0;*/
 
 self.addEventListener('message', async function (e) {
     if (workerState === StateEnum.Created) {
@@ -137,14 +83,6 @@ self.addEventListener('message', async function (e) {
             // Expected to finish some promises with abort error
             // The abort "errors" are already sent to main "thead" by sendMessageToMain inside the promise
         }
-
-        // Remove all elements from sending queues
-        while (queue['audio'].length > 0) {
-            queue['audio'].shift();
-        }
-        while (queue['video'].length > 0) {
-            queue['video'].shift();
-        }
         return;
     }
 
@@ -154,12 +92,6 @@ self.addEventListener('message', async function (e) {
             return;
         }
 
-        if ('audioMaxMaxQueueSizeMs' in e.data.muxerSenderConfig) {
-            audioMaxMaxQueueSizeMs = e.data.muxerSenderConfig.audioMaxMaxQueueSizeMs;
-        }
-        if ('videoMaxMaxQueueSizeMs' in e.data.muxerSenderConfig) {
-            videoMaxMaxQueueSizeMs = e.data.muxerSenderConfig.videoMaxMaxQueueSizeMs;
-        }
         if ('maxInFlightAudioRequests' in e.data.muxerSenderConfig) {
             maxFlightRequests['audio'] = e.data.muxerSenderConfig.maxInFlightAudioRequests;
         }
@@ -211,53 +143,28 @@ self.addEventListener('message', async function (e) {
     }
 
     const chunkData = { mediaType: mediaType, firstFrameClkms: e.data.firstFrameClkms, compesatedTs: e.data.compesatedTs, estimatedDuration: e.data.estimatedDuration, seqId: e.data.seqId, maxAgeChunkS: maxAgeChunkS, chunk: e.data.chunk, metadata: e.data.metadata };
-    if (type === "vchunk") {
-        isVideoPresent = true;
-        if (getQueueLengthMs(queue[mediaType]) >= videoMaxMaxQueueSizeMs) {
-            // We drop all the queue we want low latency over quality (circular queue)
-            while (queue[mediaType].length > 0) {
-                vChunkDataDropped = queue[mediaType].shift();
-                sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), seqId: vChunkDataDropped.seqId, mediaType: vChunkDataDropped.mediaType, ts: vChunkDataDropped.chunk.timestamp, msg: "Dropped video chunk (all queue)" });
-            }
-            videoNextNeedsKey = true; // Since we do not know the relation between delta frames we wait for I frame when we drop just 1 video frame
-            if (isAudioPresent) {
-                videoPenaltyAudioSentFrames = VIDEO_PENALTY_SENT_FRAMES;
-            }
-            // TODO: JOC Improvement tell encoder to insert a keyframe
-        } else {
-            if (videoPenaltyAudioSentFrames > 0) {
-                sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), seqId: chunkData.seqId, mediaType: chunkData.mediaType, ts: chunkData.chunk.timestamp, msg: "Dropped video chunk (penalty)" });
-            } else if (videoNextNeedsKey && chunkData.chunk.type != "key") {
-                sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), seqId: chunkData.seqId, mediaType: chunkData.mediaType, ts: chunkData.chunk.timestamp, msg: "Dropped video chunk (waiting key)" });
-            } else {
-                queue[mediaType].push(chunkData);
-                videoNextNeedsKey = false;
-            }
-        }
+    if (mediaType == "audio") {
+        sendChunkToTransport(chunkData, inFlightRequests['audio'], maxFlightRequests['audio'], packagerVersion);
+    } else {
+        sendChunkToTransport(chunkData, inFlightRequests['video'], maxFlightRequests['video'], packagerVersion);
     }
-    else if (type === "achunk") {
-        isAudioPresent = true;
-        if (getQueueLengthMs(queue[mediaType]) >= audioMaxMaxQueueSizeMs) {
-            // We drop all the queue we want low latency over quality and minimize glitches
-            while (queue[mediaType].length > 0) {
-                aChunkDataDropped = queue[mediaType].shift();
-                sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), seqId: aChunkDataDropped.seqId, mediaType: aChunkDataDropped.mediaType, ts: aChunkDataDropped.chunk.timestamp, msg: "Dropped audio chunk (all queue)" });
-            }
-        } else {
-            if (videoPenaltyAudioSentFrames > 0) {
-                videoPenaltyAudioSentFrames--;
-            }
-            queue[mediaType].push(chunkData);
-        }
-    }
-
-    sendData();
 
     // Report stats
-    self.postMessage({ type: "sendstats", clkms: Date.now(), inFlightAudioReqNum: getInflightRequestsLength(inFlightRequests['audio']), inFlightVideoReqNum: getInflightRequestsLength(inFlightRequests['video']), audioQueueStats: getQueueStats(queue['audio']), videoQueueStats: getQueueStats(queue['video']), efficiencyData: efficiencyData });
+    self.postMessage({ type: "sendstats", clkms: Date.now(), inFlightAudioReqNum: getInflightRequestsLength(inFlightRequests['audio']), inFlightVideoReqNum: getInflightRequestsLength(inFlightRequests['video']), efficiencyData: efficiencyData });
 
     return;
 });
+
+function sendChunkToTransport(chunkData, inFlightRequests, maxFlightRequests, packagerVersion) {
+    if (chunkData == null) {
+        return;
+    }
+    if (getInflightRequestsLength(inFlightRequests) >= maxFlightRequests) {
+        sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), seqId: chunkData.seqId, mediaType: chunkData.mediaType, ts: chunkData.timestamp, msg: "Dropped chunk because too many inflight requests" });
+        return;
+    }
+    return createRequests(chunkData, packagerVersion);
+}
 
 function getInflightRequestsLength(inFlightRequestsType) {
     return Object.keys(inFlightRequestsType).length;
@@ -268,25 +175,6 @@ function getAllInflightRequestsArray() {
     const arrVideo = Object.values(inFlightRequests['video']);
 
     return arrAudio.concat(arrVideo)
-}
-
-function sendData() {
-    sendDataFromQueue(queue['audio'], inFlightRequests['audio'], maxFlightRequests['audio'], packagerVersion);
-    sendDataFromQueue(queue['video'], inFlightRequests['video'], maxFlightRequests['video'], packagerVersion);
-}
-
-function sendDataFromQueue(queue, inFlightRequests, maxFlightRequests, packagerVersion) {
-    const ret = [];
-    while ((queue.length > 0) && (getInflightRequestsLength(inFlightRequests) < maxFlightRequests)) {
-        const chunkData = queue.shift();
-        if (chunkData !== null) {
-            const requests = createRequests(chunkData, packagerVersion);
-            requests.forEach(p => {
-                ret.push(p);
-            });
-        }
-    }
-    return ret;
 }
 
 function createRequests(chunkData, packagerVersion) {
@@ -353,16 +241,6 @@ async function createWebTransportSession(url) {
         });
 }
 
-function convertToUint64BE(n) {
-    const b = new Uint8Array(8);
-    let src = n;
-    for (let n = 7; n >= 0; n--) {
-        b[n] = src & 0xFF;
-        src = src >> 8;
-    }
-    return b;
-}
-
 async function createWebTransportRequestPromise(firstFrameClkms, mediaType, chunkType, timestamp, duration, seqId, maxAgeChunkS, dataBytes, packagerVersion) {
     if (wTtransport === null) {
         sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), ts: timestamp, msg: "Dropped " + mediaType + "chunk because server error response" });
@@ -419,16 +297,12 @@ async function createWebTransportRequestPromise(firstFrameClkms, mediaType, chun
         .then(val => {
             sendMessageToMain(WORKER_PREFIX, "debug", "sent: 200. For " + mediaType + "-" + seqId + "-" + timestamp + "-" + duration + "-" + chunkType + "-" + firstFrameClkms);
             removeFromInflight(mediaType, pId);
-
-            sendData();
         })
         .catch(err => {
             sendMessageToMain(WORKER_PREFIX, "dropped", { clkms: Date.now(), ts: timestamp, msg: "Dropped " + mediaType + "chunk because sending chunk error" });
             sendMessageToMain(WORKER_PREFIX, "error", "request: " + mediaType + "-" + seqId + ". Err: " + err.message);
 
             removeFromInflight(mediaType, pId);
-
-            sendData();
         });
     return p;
 }
