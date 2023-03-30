9f9285f0873c75a1ce8cdf0f48c2297873c36bdf
Jordi Cenzano Ferret <jcenzano@fb.com>
2023-03-30 21:44:42 +0900

    Fixed latency measure

--- utils/time_checker.js
+++ utils/time_checker.js
@@ -5,25 +5,10 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-class returnWhen {
-    // Create new instances of the same class as static attributes
-    static GreaterOrEqual = new returnWhen("GreaterOrEqual")
-    static LowerOrEqual = new returnWhen("LowerOrEqual")
-    
-    constructor(name) {
-        this.name = name
-    }
-}
-
 class TimeChecker {
-    constructor(mediaType, returnIfGreated) {
+    constructor(mediaType) {
         this.mediaType = mediaType;
         this.elementsList = [];
-
-        this.mode = returnWhen.GreaterOrEqual;
-        if (returnIfGreated === returnWhen.LowerOrEqual) {
-            this.mode = returnWhen.LowerOrEqual;
-        }
     }
 
     AddItem(item) {
@@ -39,7 +24,7 @@ class TimeChecker {
 
         // elementsList is sorted by arrival order
         while ((ret === undefined) && (i < this.elementsList.length)) {
-            if (this.checkCondtion(this.elementsList[i].ts, ts)) {
+            if (this.elementsList[i].ts >= ts) {
                 ret = this.elementsList[i];
                 // Remove all previous frames data
                 this.elementsList = this.elementsList.slice(i + 1);
@@ -48,9 +33,9 @@ class TimeChecker {
         }
 
         /*if (ret === undefined) {
-            console.log("JOC elements list: " + this.elementsList.length + ", retTs: undefined, asked: " + ts + ", " + JSON.stringify(this.elementsList))
+            console.log("JOC elements list: " + this.elementsList.length + ", retTs: undefined, asked: " + ts + ", " + JSON.stringify(this.elementsList) + ", mode: " + this.mode)
         } else {
-            console.log("JOC elements list: " + this.elementsList.length + ", retTs: " + ret.ts + ", asked: " + ts + ", " + JSON.stringify(this.elementsList))
+            console.log("JOC elements list: " + this.elementsList.length + ", retTs: " + ret.ts + ", asked: " + ts + ", " + JSON.stringify(this.elementsList) + ", mode: " + this.mode)
         }*/
 
         return ret;
@@ -58,15 +43,5 @@ class TimeChecker {
 
     Clear() {
         this.elementsList = [];
-    }
-
-    checkCondtion(elementTs, ts) {
-        let ret = false;
-        if (this.mode === returnWhen.GreaterOrEqual) {
-            ret = elementTs >= ts;
-        } else {
-            ret = elementTs <= ts;
-        }
-        return ret;
     }
 }
